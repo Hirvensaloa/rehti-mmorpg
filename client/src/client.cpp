93bcbf1c8bfd173ab7a270d59af272038e011ff9
Samu Kähkönen <samuv.kahkonen@gmail.com>
2023-09-11 17:15:52 +0300

    update client build (added conan)

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,6 +1,8 @@
 #include "graphics.h"
+#include "network.h"
 #include <iostream>
 int main() {
 	testFunc(); // should be available in test.hpp. That in turn includes glfw library
+	testNetwork();
 	std::cout << "Winner winner chicken dinner" << std::endl;
 }

4b65a7a49db0d66ebc6d86d232448652ce92efe0
Samu Kähkönen <samuv.kahkonen@gmail.com>
2023-09-11 17:17:54 +0300

    add base for network library

---
+++ rehtiLib/network/include/network.h
@@ -0,0 +1,2 @@
+#include "../src/networkBase.hpp"
+
---
+++ rehtiLib/network/src/networkBase.cpp
@@ -0,0 +1,8 @@
+#include "networkBase.hpp"
+#include <iostream>
+
+ int testNetwork() {
+    boost::asio::io_context io_context;
+    std::cout << "winning!" << std::endl;
+	return 0;
+}

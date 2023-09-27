2bb366549196cc1d2a5589ee7dd82376f39fdee0
Hirvensaloa <76909515+Hirvensaloa@users.noreply.github.com>
2023-09-27 20:10:41 +0300

    fix: script path (#12)

--- scripts/run-backend-conan.sh
+++ scripts/run-backend-conan.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
+    # Linux
+    EXECUTABLE_PATH="./server/build/src/server"
+elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" || "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]]; then
+    # Windows (MINGW32_NT or MINGW64_NT)
+    EXECUTABLE_PATH="./server/build/Debug/src/server"
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
+
 ./scripts/build-backend-conan.sh
 
-./server/build/src/server
+if [ -f "$EXECUTABLE_PATH" ]; then
+    echo "Executing $EXECUTABLE_PATH"
+    "$EXECUTABLE_PATH"
+else
+    echo "Executable not found at $EXECUTABLE_PATH"
+    exit 1
+fi

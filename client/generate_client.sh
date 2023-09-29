8ddcee31bda3a11a9d04fb2c656938b518c1ae3a
OsoWiz <otso.laasonen@gmail.com>
2023-09-29 15:28:55 +0300

    chore: adding vulkan loader. NOT LINKING CURRENTLY

--- client/generate_client.sh
+++ client/generate_client.sh
@@ -1,3 +1,3 @@
-conan install . --output-folder=build --build=missing
+conan install . --output-folder=build --build=missing -s build_type=Debug
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug

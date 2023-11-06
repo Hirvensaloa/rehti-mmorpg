3d0006f71b4575e025c84a7b9f10be19ed2e9d3d
OsoWiz <otso.laasonen@gmail.com>
2023-11-07 01:01:14 +0200

    fix: added test texture to be full grey, and forced texture loader to 4 channels.

--- rehtiLib/assets/reader/src/TextureReader.cpp
+++ rehtiLib/assets/reader/src/TextureReader.cpp
@@ -10,32 +10,32 @@
 
 std::map<std::string, StbImageData> loadTextures()
 {
-  std::map<std::string, StbImageData> textures;
+	std::map<std::string, StbImageData> textures;
 
-  int width, height, channels;
-  unsigned char *data;
+	int width, height, channels;
+	unsigned char* data;
 
-  // Read all the texture filenames
-  std::vector<std::string> textureFilenames;
-  for (const auto &entry : std::filesystem::directory_iterator(Config.TEXTURE_PATH))
-  {
-    textureFilenames.push_back(entry.path().filename().string());
-  }
+	// Read all the texture filenames
+	std::vector<std::string> textureFilenames;
+	for (const auto& entry : std::filesystem::directory_iterator(Config.TEXTURE_PATH))
+	{
+		textureFilenames.push_back(entry.path().filename().string());
+	}
 
-  // Read all the textures
-  for (const auto &filename : textureFilenames)
-  {
-    data = stbi_load((Config.TEXTURE_PATH + filename).c_str(), &width, &height, &channels, 0);
-    if (data)
-    {
-      textures[filename] = StbImageData{data, width, height};
-      std::cout << "Loaded texture: " << filename << std::endl;
-    }
-    else
-    {
-      std::cout << "Failed to load texture: " << filename << std::endl;
-    }
-  }
+	// Read all the textures
+	for (const auto& filename : textureFilenames)
+	{
+		data = stbi_load((Config.TEXTURE_PATH + filename).c_str(), &width, &height, &channels, 4);
+		if (data)
+		{
+			textures[filename] = StbImageData{ data, width, height };
+			std::cout << "Loaded texture: " << filename << std::endl;
+		}
+		else
+		{
+			std::cout << "Failed to load texture: " << filename << std::endl;
+		}
+	}
 
-  return textures;
+	return textures;
 }

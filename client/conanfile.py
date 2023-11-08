1c0eef500b0a8e5f179a1e0f82287af723506684
Samu Kähkönen <samuv.kahkonen@gmail.com>
2023-11-08 13:15:48 +0200

    add vulkan and glfw bindings for imgui

--- client/conanfile.py
+++ client/conanfile.py
@@ -1,4 +1,8 @@
 from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+from conan.tools.files import copy
+
+import os
 
 class ClientConan(ConanFile):
     name = "Client"
@@ -18,3 +22,11 @@ class ClientConan(ConanFile):
         self.requires("vulkan-headers/1.3.239.0", override=True)
         self.requires("imgui/cci.20230105+1.89.2.docking")
 
+    def generate(self):
+        copy(self, "*glfw*", os.path.join(self.dependencies["imgui"].package_folder,
+            "res", "bindings"), os.path.join(self.source_folder, "../rehtiLib/graphics/bindings"))
+        copy(self, "*vulkan*", os.path.join(self.dependencies["imgui"].package_folder,
+            "res", "bindings"), os.path.join(self.source_folder, "../rehtiLib/graphics/bindings"))
+
+    def layout(self):
+        cmake_layout(self)

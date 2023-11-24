a1fa7ea80ac04b92a4c2ae98669cc23e14e075df
OsoWiz <otso.laasonen@gmail.com>
2023-11-24 16:46:49 +0200

    feat: animation interface created

--- rehtiLib/graphics/src/TimerCallbackSystem.cpp
+++ rehtiLib/graphics/src/TimerCallbackSystem.cpp
@@ -2,43 +2,44 @@
 
 void TimerCallbackSystem::elapseTime(float dt)
 {
-	for (auto it = timersM.begin(); it != timersM.end();)
-	{
-		auto& timer = it->second;
-		float elapsedTime = std::min(timer.time, dt);
-		timer.callback(elapsedTime);
-		timer.time -= elapsedTime;
-		if (abs(timer.time) < EPSILON)
-		{
-			timersM.erase(it++);
-		}
-		else
-		{
-			++it;
-		}
-	}
+    for (auto it = timersM.begin(); it != timersM.end();)
+    {
+        auto& timer = it->second;
+        float deltaTime = dt * timer.factor;
+        float elapsedTime = std::min(timer.time, deltaTime);
+        timer.callback(elapsedTime);
+        timer.time -= elapsedTime;
+        if (abs(timer.time) < EPSILON)
+        {
+            timersM.erase(it++);
+        }
+        else
+        {
+            ++it;
+        }
+    }
 }
 
-void TimerCallbackSystem::addTimerCallback(int id, float time, std::function<void(float dt)> callback)
+void TimerCallbackSystem::addTimerCallback(int id, float time, std::function<void(float dt)> callback, float factor)
 {
-	timersM[id] = { time, callback };
+    timersM[id] = {time, factor, callback};
 }
 
 bool TimerCallbackSystem::forceQuitCallback(int id)
 {
-	size_t elementsRemoved = timersM.erase(id);
-	return 0 < elementsRemoved;
+    size_t elementsRemoved = timersM.erase(id);
+    return 0 < elementsRemoved;
 }
 
 bool TimerCallbackSystem::finishCallback(int id)
 {
-	auto it = timersM.find(id);
-	if (it != timersM.end())
-	{
-		// call the callback with the remaining time
-		it->second.callback(it->second.time);
-		timersM.erase(it);
-		return true;
-	}
-	return false;
+    auto it = timersM.find(id);
+    if (it != timersM.end())
+    {
+        // call the callback with the remaining time
+        it->second.callback(it->second.time);
+        timersM.erase(it);
+        return true;
+    }
+    return false;
 }

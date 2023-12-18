9f6181f22911d1b360496ca34bb08275180da98e
Aleksi Hirvensalo <aleksi.hirvensalo@aalto.fi>
2023-12-18 20:28:51 +0200

    chore: db port from env

--- server/src/database/DatabaseManager.cpp
+++ server/src/database/DatabaseManager.cpp
@@ -8,6 +8,7 @@ const char* DB_NAME = std::getenv("POSTGRES_DB");
 const char* DB_USER = std::getenv("POSTGRES_USER");
 const char* DB_PASSWORD = std::getenv("POSTGRES_PASSWORD");
 const char* DB_HOST = std::getenv("DB_HOST");
+const char* DB_PORT = std::getenv("DB_PORT");
 
 DatabaseManager::DatabaseManager() : pConnectionM{nullptr}
 {
@@ -20,7 +21,7 @@ int DatabaseManager::createConnection()
     {
         // Connect to the db
         std::stringstream connectionParams;
-        connectionParams << "dbname=" << DB_NAME << " user=" << DB_USER << " password=" << DB_PASSWORD << " host=" << DB_HOST << " port=5432"
+        connectionParams << "dbname=" << DB_NAME << " user=" << DB_USER << " password=" << DB_PASSWORD << " host=" << DB_HOST << " port=" << DB_PORT
                          << " connect_timeout=10";
         pConnectionM = std::make_unique<pqxx::connection>(connectionParams.str());
         std::cout << "Connected to " << pConnectionM->dbname() << '\n';
